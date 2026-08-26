ef3cbb0f670deb0958306dbe7949233d76ba756e
phtnnz <git16@mj.m-j-s.net>
2026-08-26 22:03:29 +0200

    Added test case for pyfo

---
+++ tests/test_find_orb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Copyright 2026 Martin Junius
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ChangeLog
+# Version 0.1 / 2026-08-26
+#       Test neoop.pyfo.pyfo
+
+from icecream import ic
+
+from neoop.mpc.observations import Obs
+from neoop.neo.local import LocalCircumstances
+from neoop.pyfo.pyfo import FindOrb
+
+ic.enable()
+obj = "2026 NT2"
+
+# Observer location and local circumstances
+local = LocalCircumstances.from_location("M49")
+ic(local)
+
+fo = FindOrb()
+ic(fo)
